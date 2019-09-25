82f698bdc43d0b2c354eb0d1cdcb5907ad499bbf
Shamsiel <lyricquinn27@gmail.com>
2019-09-25 05:51:09 -0300

    pkm

--- pokemon_combat_game.py
+++ pokemon_combat_game.py
@@ -20,31 +20,35 @@ while pikachu_life > 0 and enemy_life > 0:
     if move_input == 1:
         enemy_life -= 12
         print('Pikachu uses Thunder Shock and inflicts 12 points of damage')
-        print('{} Life = {}'.format(chosen_pokemon,(enemy_life)).lower().capitalize())
-        print('Pikachu Life = {}'.format(pikachu_life))
+        print('{}\'s Life = {}'.format(chosen_pokemon,(enemy_life)).lower().capitalize())
+        print('Pikachu\'s Life = {}'.format(pikachu_life))
 
     elif move_input == 2:
         enemy_life -= 10
         print('Pikachu uses Tail Whip and inflicts 10 points of damage')
-        print('{} Life = {}'.format(chosen_pokemon, (enemy_life)).lower().capitalize())
-        print('Pikachu Life = {}'.format(pikachu_life))
+        print('{}\'s Life = {}'.format(chosen_pokemon, (enemy_life)).lower().capitalize())
+        print('Pikachu\'s Life = {}'.format(pikachu_life))
 
     if chosen_pokemon == 'SQUIRTLE':
         print('Squirtle hits you and inflicts 8 damage points')
         pikachu_life -= 8
-        print('{} Life = {}'.format(chosen_pokemon, (enemy_life)).lower().capitalize())
-        print('Pikachu Life = {}'.format(pikachu_life))
+        print('{}\'s Life = {}'.format(chosen_pokemon, (enemy_life)).lower().capitalize())
+        print('Pikachu\'s Life = {}'.format(pikachu_life))
 
     if chosen_pokemon == 'CHARMANDER':
         print('Charmander attacks you and inflicts 10 damage points')
         pikachu_life -= 10
-        print('{} Life = {}'.format(chosen_pokemon, (enemy_life)).lower().capitalize())
-        print('Pikachu Life = {}'.format(pikachu_life))
+        print('{}\'s Life = {}'.format(chosen_pokemon, (enemy_life)).lower().capitalize())
+        print('Pikachu\'s Life = {}'.format(pikachu_life))
     if chosen_pokemon == 'BULBASAUR':
         random_damage_number = random.randint(8,12)
         print('Bulbasaur strikes you and inflicts {} points of damage'.format(random_damage_number))
         pikachu_life -= random_damage_number
-        print('{} Life = {}'.format(chosen_pokemon, (enemy_life)).lower().capitalize())
-        print('Pikachu Life = {}'.format(pikachu_life))
+        print('{}\'s Life = {}'.format(chosen_pokemon, (enemy_life)).lower().capitalize())
+        print('Pikachu\'s Life = {}'.format(pikachu_life))
 
 print('Combat Finished.')
+if pikachu_life <= 0:
+    print('Pikachu is the Winner')
+elif enemy_life <= 0:
+    print('{} is the Winner'.format(chosen_pokemon).lower().capitalize())
